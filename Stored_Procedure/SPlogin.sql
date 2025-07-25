30f6d845527686706c7fac47a1e2c2bb0bb7d48f
Nikhil1210-dev <nikhilnavale2006@gmail.com>
2025-07-25 12:25:18 +0530

    SPlogin

---
+++ Stored_Procedure/SPlogin.sql
@@ -0,0 +1,12 @@
+-- 2. Creating a Stored Procedure for User Login
+
+use tflecommerce;
+select * from tflecommerce.users;
+delimiter //
+create procedure userlogin(in pusername varchar(98),in spassword varchar(90))
+begin
+select  username,password from users where username=pusername
+and password=spassword;
+end //
+call userlogin('nikhil n','nik780');
+select * from users;
